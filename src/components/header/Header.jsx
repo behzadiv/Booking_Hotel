75f0a26e3c848bb007120f031b0e3403248b8a62
behzad <90694618+behzadiv@users.noreply.github.com>
2023-09-06 15:07:28 +0330

    feat : develope initial guestOption section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,40 +1,76 @@
 import { MdLocationOn } from "react-icons/md";
-import { HiCalendar, HiSearch } from "react-icons/hi";
+import { HiCalendar, HiSearch, HiMinus, HiPlus } from "react-icons/hi";
 import { useState } from "react";
 
 const Header = () => {
   const [destination, setDestination] = useState("");
+  const [openOption, setOpenOption] = useState(false);
 
   return (
-    <div className="headerSearch">
-      <div className="headerSearchItem ">
-        <MdLocationOn className="locationIcon" />
-        <input
-          type="text"
-          name="destination"
-          placeholder="Where to go?"
-          onChange={(e) => setDestination(e.target.value)}
-        />
-        <span className="seperator"></span>
-      </div>
-      <div className="headerSearchItem">
-        <HiCalendar className="headerIcon dateIcon" />
-        <div className="dateDropDown">2023/03/20</div>
-        <span className="seperator"></span>
-      </div>
-      <div className="headerSearchItem">
-        <div className="optionDropDown">
-          1adult &bull; 2children &bull;2room
+    <div className="header">
+      <div className="headerSearch">
+        <div className="headerSearchItem ">
+          <MdLocationOn className="locationIcon" />
+          <input
+            type="text"
+            name="destination"
+            placeholder="Where to go?"
+            onChange={(e) => setDestination(e.target.value)}
+          />
+          <span className="seperator"></span>
         </div>
-        <span className="seperator"></span>
-      </div>
-      <div className="headerSearchItem">
-        <button className="headerSearchBtn">
-          <HiSearch className="headerIcon" />
-        </button>
+        <div className="headerSearchItem">
+          <HiCalendar className="headerIcon dateIcon" />
+          <div className="dateDropDown">2023/03/20</div>
+          <span className="seperator"></span>
+        </div>
+        <div className="headerSearchItem">
+          <div
+            className="optionDropDown"
+            onClick={() => {
+              setOpenOption(!openOption);
+            }}
+          >
+            1adult &bull; 2children &bull;2room
+          </div>
+          {openOption && <GuestOption />}
+          <span className="seperator"></span>
+        </div>
+        <div className="headerSearchItem">
+          <button className="headerSearchBtn">
+            <HiSearch className="headerIcon" />
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Header;
+
+function GuestOption() {
+  return (
+    <div className="guestOptions">
+      <OptionItem />
+      <OptionItem />
+      <OptionItem />
+    </div>
+  );
+}
+
+function OptionItem() {
+  return (
+    <div className="guestOptionItem">
+      <span className="optionText">adult</span>
+      <div className="optionCounter">
+        <button className="optionCounterBtn">
+          <HiMinus className="icon" />
+        </button>
+        <span>1</span>
+        <button className="optionCounterBtn">
+          <HiPlus className="icon" />
+        </button>
+      </div>
+    </div>
+  );
+}
