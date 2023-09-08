2f70b69c75c805b423a52e28b241c828f8ca62b8
behzad <90694618+behzadiv@users.noreply.github.com>
2023-09-08 11:53:51 +0330

    feat : add react-router-dom to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./components/header/Header";
 import "./App.css";
 import LocationList from "./components/locationList/LocationList";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
-      <LocationList />
+      <Routes>
+        <Route path="/" element={<LocationList />} />
+      </Routes>
     </>
   );
 }
