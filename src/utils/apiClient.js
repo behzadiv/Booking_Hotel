720f095433fc21cf662a31d2ef16edd3e96b460b
behzad <90694618+behzadiv@users.noreply.github.com>
2023-09-08 15:37:12 +0330

    bugfix : fix when quers string is empty

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default function apiClient(option) {
   const makingUrl = (option) => {
-    return `${import.meta.env.VITE_BASE_URL}${option.url}${option.query}`;
+    return `${import.meta.env.VITE_BASE_URL}${option.url}${option.query||""}`;
   };
   return axios({ url: makingUrl(option), method: option.method });
 }
