963269933515673bd3b045b925ccc834a7262f91
behzad <90694618+behzadiv@users.noreply.github.com>
2023-09-09 13:27:12 +0330

    feat : install leaflet pkg and develope Map component to show locations on map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { useHotels } from "./context/HotelsContext";
 
 const Map = () => {
-   const {hotels,loading}=useHotels()
-    console.log(hotels);
-    return ( 
-        <div>
-            mapsection
-        </div>
-     );
-}
- 
+  const { hotels } = useHotels();
+  const [position, setPosition] = useState([51.02, -0.91]);
+  return (
+    <div className="mapContainer">
+      <MapContainer
+        className="map"
+        center={position}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {hotels.map((item) => {
+          return (
+            <Marker key={item.id} position={[item.latitude, item.longitude]}>
+              <Popup>{item.name}</Popup>
+            </Marker>
+          );
+        })}
+      </MapContainer>
+    </div>
+  );
+};
+
 export default Map;
