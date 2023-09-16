6a8f1fe45ef91cdc0df0a6807a2c9cf1df5125a4
behzad <90694618+behzadiv@users.noreply.github.com>
2023-09-16 17:50:04 +0330

    feat : show bookmarksList in Bookmarks component

--- src/components/bookmarks/Bookmarks.jsx
+++ src/components/bookmarks/Bookmarks.jsx
@@ -1,5 +1,26 @@
+import ReactCountryFlag from "react-country-flag";
+import { useBookmarks } from "../context/BookmarksContext";
+
 const Bookmarks = () => {
-  return <div>bookmarks Page</div>;
+  const { bookmarks, loading } = useBookmarks();
+  
+  if (loading) return <p>Loading...</p>;
+  return (
+    <>
+      <h2>Bookmarks List : </h2>
+      <div className="bookmarkList">
+        {bookmarks.map((item) => {
+          return (
+            <div className="bookmarkItem" key={item.id}>
+              <ReactCountryFlag svg countryCode={item.countryCode} />
+              &nbsp;<strong>{item.cityName}</strong>&nbsp;
+              <span>{item.country}</span>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Bookmarks;
