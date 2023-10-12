e826683aa482ef4f1bcd13d4e7b74350846f79d1
behzad <90694618+behzadiv@users.noreply.github.com>
2023-10-12 12:03:13 +0330

    feat : check login and authentication

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,17 +1,51 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPasssword] = useState("");
+  const { user, isAuthenticated, login } = useAuth();
+  const navigate = useNavigate();
+  const loginHandler = (e) => {
+    e.preventDefault();
+    if (email && password) login(email, password);
+    console.log(user);
+  };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/", { replace: true });
+    }
+  }, [isAuthenticated, user]);
+
   return (
     <div className="loginContainer">
       <h2>Login</h2>
       <form className="form">
         <div className="formControl">
           <label htmlFor="email">Email : </label>
-          <input type="text" name="email" id="email"/>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div className="formControl">
           <label htmlFor="password">Password : </label>
-          <input type="text" name="password" id="password"/>
+          <input
+            type="password"
+            name="password"
+            required
+            id="password"
+            onChange={(e) => setPasssword(e.target.value)}
+          />
         </div>
-        <button className="btn btn--primary">login</button>
+        <button className="btn btn--primary" onClick={(e) => loginHandler(e)}>
+          login
+        </button>
       </form>
     </div>
   );
